1cc1145218944782ec0ff00ae4f8616746c7ab46
Lara <volynkinala@gmail.com>
2022-08-01 11:50:54 +0400

    feat: guessing game solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.minNumber = 0;
+    this.maxNumber = 0;
+    this.guessNumber = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.minNumber = min;
+    this.maxNumber = max;
+  }
 
-    }
+  guess() {
+    this.guessNumber = Math.round((this.minNumber + this.maxNumber) / 2);
+    return this.guessNumber;
+  }
 
-    guess() {
+  lower() {
+    this.maxNumber = Math.round((this.minNumber + this.maxNumber) / 2);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.minNumber = Math.round((this.minNumber + this.maxNumber) / 2);
+  }
 }
 
 module.exports = GuessingGame;
